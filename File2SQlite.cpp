fcd8e9da1cadca9b77ddacb0d59b951f01e5c463
chen <chen@example.com>
2019-10-25 23:42:15 +0800

    clean the code and write the readme

--- File2SQlite.cpp
+++ File2SQlite.cpp
@@ -1,8 +1,4 @@
-// ConsoleApplication16.cpp : ¶¨Òå¿ØÖÆÌ¨Ó¦ÓÃ³ÌĞòµÄÈë¿Úµã¡£
-//
 
-// test1.cpp : ¶¨Òå¿ØÖÆÌ¨Ó¦ÓÃ³ÌĞòµÄÈë¿Úµã¡£
-//
 #include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,26 +20,24 @@
 
 const int Length = 710;
 static int count = 0;
+std::string SQlitedb="test2.db";
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 	int i;
+	//test whether the data is right or not 
 	for (i = 0; i < argc; i++){
 		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
 		if (i == 3){
 			float* f = new float[Length];
 			memmove(f, argv[i], Length*4);
-			printf("%d\n", sizeof(float));
-			printf("this is Hello");
 			for (int i = 0; i < Length; i++){
-				//printf("%f\n", f[i]);
+				printf("%f\n", f[i]);
 			}
-			
 		}
 	}
 	printf("\n");
 	return 0;
 }
-
 void CreateTable(){
 	sqlite3 *db;
 	int rc;
@@ -51,7 +45,7 @@ void CreateTable(){
 	char *zErrMsg = 0;
 
 	/* Open database */
-	rc = sqlite3_open("test2.db", &db);
+	rc = sqlite3_open(SQlitedb, &db);
 	if (rc){
 		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
 		exit(0);
@@ -59,7 +53,7 @@ void CreateTable(){
 	else{
 		fprintf(stdout, "Opened database successfully\n");
 	}
-
+	//create the table and the elements are ID,Position,MatrixH,Fading
 	sql = "CREATE TABLE ChannelMap("  \
 		"ID  INT PRIMARY KEY  NOT NULL," \
 		"Position TEXT NOT NULL,"\
@@ -79,55 +73,6 @@ void CreateTable(){
 
 }
 
-void InsertElement(int begin,int end){
-	sqlite3 *db;
-	int rc;
-	char *sql;
-	char *zErrMsg = 0;
-	
-	/* Open database */
-	rc = sqlite3_open("test1.db", &db);
-
-	for (int j = begin; j < end; j++){
-		
-		if (rc){
-			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-			exit(0);
-		}
-		else{
-			fprintf(stderr, "Opened database successfully\n");
-		}
-		printf("%d\n", j);
-		float* f = new float[Length];
-		for (int i = 0; i < Length; i++){
-			f[i] = i / 1000.0f;
-		}
-		sqlite3_stmt * stat;
-		sqlite3_prepare(db, "INSERT INTO ChannelMap(ID,Position,MatrixH,Fading) VALUES(?,?,?, 20000.00 )", -1, &stat, 0);
-		sqlite3_bind_int(stat, 1, j);
-
-		std::string str = std::to_string(j);
-		sqlite3_bind_text(stat, 2, str.c_str(), strlen(str.data()), NULL);
-
-
-		//std::string str = std::to_string(j);
-		//const char *tmp = str.c_str();
-		////str1= str.data();
-		//sqlite3_bind_text(stat, 2, tmp,-1, SQLITE_STATIC);
-
-		//sqlite3_bind_int(stat, 3, j);
-		//sqlite3_bind_text(stat, 2, m1.c_str(), strlen(m1.data()), NULL);
-		//sqlite3_bind_text(stat, 3, m2.c_str(), strlen(m2.data()), NULL);
-
-		sqlite3_bind_blob(stat, 3, f, Length * 4, NULL); // pdataÎªÊı¾İ»º³åÇø£¬length_of_data_in_bytesÎªÊı¾İ´óĞ¡£¬ÒÔ×Ö½ÚÎªµ¥Î»
-		int result = sqlite3_step(stat);
-		sqlite3_finalize(stat); //°Ñ¸Õ²Å·ÖÅäµÄÄÚÈİÎö¹¹µô
-		
-	}
-	sqlite3_close(db);
-
-}
-
 void InsertElement(char filename[1315], float MatrixH[356][2]){
 	sqlite3 *db;
 	int rc;
@@ -137,28 +82,26 @@ void InsertElement(char filename[1315], float MatrixH[356][2]){
 	/* Open database */
 	rc = sqlite3_open("test2.db", &db);
 
-		if (rc){
-			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-			exit(0);
-		}
-		else{
-			fprintf(stderr, "Opened database successfully\n");
-		}
-		float* f = new float[Length];
-		for (int i = 0; i < Length; i++){
-			f[i] = MatrixH[i/2][i%2];
-		}
-		sqlite3_stmt * stat;
-		sqlite3_prepare(db, "INSERT INTO ChannelMap(ID,Position,MatrixH,Fading) VALUES(?,?,?,?)", -1, &stat, 0);
-		sqlite3_bind_int(stat, 1, count++);
+	if (rc){
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		exit(0);
+	}
+	else{
+		fprintf(stderr, "Opened database successfully\n");
+	}
+	float* f = new float[Length];
+	for (int i = 0; i < Length; i++){
+		f[i] = MatrixH[i/2][i%2];
+	}
+	sqlite3_stmt * stat;
+	sqlite3_prepare(db, "INSERT INTO ChannelMap(ID,Position,MatrixH,Fading) VALUES(?,?,?,?)", -1, &stat, 0);
+	sqlite3_bind_int(stat, 1, count++);
+	sqlite3_bind_text(stat, 2, filename, strlen(filename), NULL);
 
-		//std::string str = std::to_string(filename);
-		sqlite3_bind_text(stat, 2, filename, strlen(filename), NULL);
-
-		sqlite3_bind_blob(stat, 3, f, Length * 4, NULL); // pdataÎªÊı¾İ»º³åÇø£¬length_of_data_in_bytesÎªÊı¾İ´óĞ¡£¬ÒÔ×Ö½ÚÎªµ¥Î»
-		sqlite3_bind_double(stat, 4, MatrixH[355][0]);
-		int result = sqlite3_step(stat);
-		sqlite3_finalize(stat); //°Ñ¸Õ²Å·ÖÅäµÄÄÚÈİÎö¹¹µô
+	sqlite3_bind_blob(stat, 3, f, Length * 4, NULL); // store MatrixH
+	sqlite3_bind_double(stat, 4, MatrixH[355][0]);
+	int result = sqlite3_step(stat);
+	sqlite3_finalize(stat); //rsset stat
 
 	sqlite3_close(db);
 
@@ -169,15 +112,14 @@ void listFiles(const char * dir)
 {
 	char dirNew[200];
 	strcpy(dirNew, dir);
-	strcat(dirNew, "\\*.*");    // ÔÚÄ¿Â¼ºóÃæ¼ÓÉÏ"\\*.*"½øĞĞµÚÒ»´ÎËÑË÷
+	strcat(dirNew, "\\*.*");    // åœ¨ç›®å½•åé¢åŠ ä¸Š"\\*.*"è¿›è¡Œç¬¬ä¸€æ¬¡æœç´¢
 
 	intptr_t handle;
 	_finddata_t findData;
 
 	handle = _findfirst(dirNew, &findData);
-	if (handle == -1)        // ¼ì²éÊÇ·ñ³É¹¦
+	if (handle == -1)        // æ£€æŸ¥æ˜¯å¦æˆåŠŸ
 		return;
-
 	do
 	{
 		if (findData.attrib & _A_SUBDIR)
@@ -187,21 +129,20 @@ void listFiles(const char * dir)
 
 			std::cout << findData.name << "\t<dir>\n";
 
-			// ÔÚÄ¿Â¼ºóÃæ¼ÓÉÏ"\\"ºÍËÑË÷µ½µÄÄ¿Â¼Ãû½øĞĞÏÂÒ»´ÎËÑË÷
+			// åœ¨ç›®å½•åé¢åŠ ä¸Š"\\"å’Œæœç´¢åˆ°çš„ç›®å½•åè¿›è¡Œä¸‹ä¸€æ¬¡æœç´¢ï¼ˆåœ¨å­ç›®å½•ä¸­è¿›è¡Œæœç´¢ï¼‰
 			strcpy(dirNew, dir);
 			strcat(dirNew, "\\");
 			strcat(dirNew, findData.name);
-
 			listFiles(dirNew);
 		}
 		else{
 			std::cout << findData.name << "\t" << "\n";
-			char filename[1315];// = findData.name;
-			char filename1[1315];
-			sprintf(filename, "D:\\download\\Base-11ad-20191024\\data3\\%s",findData.name);
-			sprintf(filename1, "%s", findData.name);
+			char path[1315];// = findData.name;
+			char filename[1315];
+			sprintf(path, "D:\\download\\Base-11ad-20191024\\data3\\%s",findData.name);
+			sprintf(filename, "%s", findData.name);
 			std::fstream _file;
-			_file.open(filename, std::ios::in);
+			_file.open(path, std::ios::in);
 			if (!_file){
 				_ASSERT(0);
 			}
@@ -209,7 +150,7 @@ void listFiles(const char * dir)
 				_file.close();
 				float aa[356][2] = { 0 };
 				FILE *file_out;
-				file_out = fopen(filename, "r");
+				file_out = fopen(path, "r");
 				//NS_ASSERT_MSG(file_out , strerror(errno));
 				for (int i = 0; i < 356; i++)
 				{
@@ -218,31 +159,22 @@ void listFiles(const char * dir)
 					//std::cout<<aa[i][0]<<" "<<aa[i][1]<<std::endl;
 				}
 				fclose(file_out);
-				InsertElement(filename1,aa);
+				InsertElement(filename,aa);
 			}
 		}
-			
-			//std::cout << findData.name << "\t" << findData.size << " bytes.\n";
 	} while (_findnext(handle, &findData) == 0);
-
-	_findclose(handle);    // ¹Ø±ÕËÑË÷¾ä±ú
+	_findclose(handle);    // å…³é—­æœç´¢å¥æŸ„
 }
 
 
 int main(int argc, char* argv[])
 {
 	char dir[200] = "D:\\download\\Base-11ad-20191024\\data3";
-	std::cout << "Enter a directory: ";
+	//std::cout << "Enter a directory: ";
 	//std::cin.getline(dir, 200);
 	CreateTable();
 
 	listFiles(dir);
-
-
-
-
-
-
 	
 	std::queue<std::thread> n;
 	sqlite3 *db;
@@ -250,68 +182,6 @@ int main(int argc, char* argv[])
 	int rc;
 	char *sql;
 	
-	/*std::thread t1(&InsertElement,0,400000);
-	std::thread t2(&InsertElement, 400001, 800000);
-	std::thread t3(&InsertElement, 800001, 1200000);
-	std::thread t4(&InsertElement, 1200001, 1600000);
-	t1.detach();
-	t2.detach();
-	t3.detach();
-	t4.detach();*/
-	//InsertElement(0,40000);
-
-
-
-	
-
-	//rc = sqlite3_open("test1.db", &db);
-	//if (rc){
-	//	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-	//	exit(0);
-	//}
-	//else{
-	//	fprintf(stdout, "Opened database successfully\n");
-	//}
-	//sql = "CREATE INDEX idx_ChannelMap ON ChannelMap(SnederX,SnederY,SnederZ,ReceiverX,ReceiverY,ReceiverZ);";
-	//rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
-	//if (rc != SQLITE_OK){
-	//	fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	//	sqlite3_free(zErrMsg);
-	//}
-	//else{
-	//	fprintf(stdout, "Table created successfully\n");
-	//}
-	//sqlite3_close(db);
-
-	///* Open database */
-	//rc = sqlite3_open("test.db", &db);
-	//if (rc){
-	//	fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-	//	exit(0);
-	//}
-	//else{
-	//	fprintf(stderr, "Opened database successfully\n");
-	//}
-
-	///* Create SQL statement */
-	//sql = "SELECT * from ChannelMap";
-
-	//const char* data = "Callback function called";
-
-	///* Execute SQL statement */
-	//rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-	//if (rc != SQLITE_OK){
-	//	fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	//	sqlite3_free(zErrMsg);
-	//}
-	//else{
-	//	fprintf(stdout, "Operation done successfully\n");
-	//}
-	//sqlite3_close(db);
-
 	while (1);
 	return 0;
 }
-
-
-
---
+++ README.md
@@ -0,0 +1,12 @@
+This project is for cnverting File to SQL db
+
+Filename is the sender position + receiver position + sender sector + receiver sector ;
+There are MatrixH and channel fading in files and MatrixH is 355x2 channel fading is in line 256. 
+
+SQL db: Table is named ChannelMap,all the elements are ID,position(filename),MatrixH,channel fading.
+MatrixH is stored in blob.
+
+TODO:
+1. simplify codes
+2. Add multithread to speed up the converting rate
+3. Try this in ubuntu 
